2552b9ffb7003336a5d8a9906d453f57831c6c42
Anthony Igbokwe <seriousta4naya@outlook.com>
2019-02-17 16:38:51 +0100

    new commit

--- src/app/common/header/header.component.ts
+++ src/app/common/header/header.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { style } from '@angular/core/src/animation/dsl';
 
 @Component({
     selector: 'bwm-header',
-    templateUrl: './header.component.html'
+    templateUrl: './header.component.html',
+    styleUrls:['./header.component.scss']
   })
   export class HeaderComponent {
     
